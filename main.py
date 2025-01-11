078bb4d344321b6e9e0d9a5942c5e45fe2956892
stephenwfields <stephenwfields@comcast.net>
2025-01-11 09:19:34 -0800

    Finished BookBot!

--- main.py
+++ main.py
@@ -1,8 +1,42 @@
+def char_count(lowered_string):
+    char_dict = {}
+
+    #Loop through string
+    for i in range(len(lowered_string)):
+
+        #Only deal with a..z characters
+        if lowered_string[i].isalpha():
+
+            #See if character is already in dictionary
+            if lowered_string[i] in char_dict:
+                #If it is, increment count
+                char_dict[lowered_string[i]] += 1
+            else:
+                #If not, add it
+                char_dict[lowered_string[i]] = 1
+    
+    #Print results, sorted in frequency used order (largest to smallest)
+ 
+    #Sort the dictonary
+    sorted_dict = {k: v for k, v in sorted(char_dict.items(), key=lambda item: item[1], reverse=True)}
+
+    #Convert to list
+    char_list = list(sorted_dict.items())
+
+   #print results
+    print("--- Begin report of books/frankenstein.txt ---")
+    for j in range(len(char_list)):
+        print(f"The '{char_list[j][0]}' character was found {char_list[j][1]} times")
+    #print(char_dict)
+
 def main():
 
     #Load book into file_content string
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
+
+    #Get character count; Force lowercase to eliminate duplicates
+    char_count(lowered_string = file_contents.lower())
 
     #Create a list of words from file_content
     words = file_contents.split()
@@ -11,6 +45,8 @@ def main():
     word_count = len(words)
     
     #Print out the word count
-    print(word_count)
+    print("")
+    print(f"{word_count} words found in the document")
+    print("--- End report ---")
 
 main()
